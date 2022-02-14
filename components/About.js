f7f1232cd6ce8c4677b90eb9f3bbd6be02950004
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2022-02-14 14:04:11 +0100

    Ajout section compétence

--- components/About.js
+++ components/About.js
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
+import Picture from "./Picture";
 
 export default function About() {
   return (
@@ -12,30 +14,53 @@ export default function About() {
           className="rounded-[1.3rem] object-cover"
         />
       </div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        accusantium cupiditate incidunt est nobis repellat soluta sapiente.
-        Cumque quaerat debitis qui, hic explicabo et aliquid unde voluptas iste
-        a optio.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        accusantium cupiditate incidunt est nobis repellat soluta sapiente.
-        Cumque quaerat debitis qui, hic explicabo et aliquid unde voluptas iste
-        a optio.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        accusantium cupiditate incidunt est nobis repellat soluta sapiente.
-        Cumque quaerat debitis qui, hic explicabo et aliquid unde voluptas iste
-        a optio.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        accusantium cupiditate incidunt est nobis repellat soluta sapiente.
-        Cumque quaerat debitis qui, hic explicabo et aliquid unde voluptas iste
-        a optio.
-      </p>
+      <div className="flex-col mx-auto text-center mt-[2rem] text-[1.2rem]">
+        <p>Antoine Jonville, 33ans, Cannes(06, Alpes-Maritimes)</p>
+        <p className="max-w-[1025px] mx-auto my-[1rem] text-[#9699A2]">
+          J&#39;ai commencé le développement web en autodidacte, étant passionné
+          par le monde de l&#39;informatique, j&#39;ai effectué une formation de
+          6 mois avec OpenClassrooms sur le parcours Développeur Web. Je suis
+          quelqu&#39;un de curieux et passionné, j&#39;aime expérimenter,
+          découvrir et apprendre au fur et à mesure de mes projets
+          professionnels et personnels.
+        </p>
+      </div>
+      <div className="flex flex-col mx-auto max-w-[1050px]">
+        <h2 className="self-center mt-[2rem] text-[1.8rem]">
+          Mes compétences :
+        </h2>
+        <p className="mt-[2rem]">
+          Vous pouvez retrouver mes projets publics en visitant mon Github, en
+          cliquant ci-dessous :
+        </p>
+        <Link href="https://github.com/Antoine-Jo">
+          <a target="_blank" rel="noreferrer" className="self-center">
+            <Image src="/github.svg" width="40" height="48" alt="github" />
+          </a>
+        </Link>
+      </div>
+
+      <div className="flex flex-wrap justify-around mx-auto mt-[3rem] max-w-[1050px]">
+        <Picture title="HTML" source="/html.svg" />
+
+        <Picture title="CSS" source="/css.svg" />
+
+        <Picture title="Sass" source="/sass.svg" />
+
+        <Picture title="JavaScript" source="/javascript.svg" />
+
+        <Picture title="React" source="/reactjs.svg" />
+
+        <Picture title="Redux" source="/redux.svg" />
+
+        <Picture title="NextJS" source="/nextjs.svg" />
+
+        <Picture title="NodeJS" source="/nodejs.svg" />
+
+        <Picture title="MongoDB" source="/mongodb.svg" />
+
+        <Picture title="MySQL" source="/mysql.svg" />
+      </div>
     </div>
   );
 }
