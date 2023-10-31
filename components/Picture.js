a0e7d17ca731f9a4be9ff9c0263d873d53951191
Antoine-Jo <antoinejonville@yahoo.fr>
2023-10-31 11:10:34 +0100

    Image optimization

--- components/Picture.js
+++ components/Picture.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 
 export default function Picture({ source, title }) {
-  return (
-    <div className="flex flex-col mb-[2rem]">
-      <Image width="100" height="100" alt="html" src={source} />
-      <h3 className="self-center text-[1rem] font-normal">{title}</h3>
-    </div>
-  );
+    return (
+        <div className="flex flex-col mb-[2rem]">
+            <Image width="100" height="100" alt="html" src={source} quality={100} />
+            <h3 className="self-center text-[1rem] font-normal">{title}</h3>
+        </div>
+    );
 }
